a8308b2581351227d1ce0e7557a6a2b5ca0484d4
Seono <pianist5555@gmail.com>
2020-06-29 03:31:51 +0900

    generic class view

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,5 +1,6 @@
 from polls import views
 from django.urls import path
+from django.views.generic import TemplateView
 
 app_name = 'polls' # 구분을 위한 namespace
 urlpatterns = [
@@ -7,4 +8,6 @@ urlpatterns = [
     path('<int:question_id>/', views.detail, name='detail'), # view 함수 호출시 views.detail(request, question_id = 'parameter')로 대응됨
     path('<int:question_id>/results', views.results, name='results'),
     path('<int:question_id>/vote', views.vote, name='vote'),
+    path('about/', MyView.as_view()) # class 클래스형으로 만들어주는 메소드 (진입메소드), 인스턴스생성 및 dispatch() 메소드 호출 역할
+    path('abouts/', TemplateView.as_view(template_name="about.html")) # 클래스형 제네릭 뷰
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from polls.models import Question, Choice
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views.generic import View, TemplateView # 클래스형 / 제네릭 템플릭 상속
 
 def index(request):
     lastest_question_list = Question.objects.all().order_by('-pub_date')[:5]
@@ -30,3 +31,40 @@ def vote(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
+class MyView(View): # 클래스형 View를 상속받는다.
+    def get(self, request):
+        # code View rogic
+        return HttpResponseRedirect('result')
+
+class AboutView(TemplateView):
+    template_name = "about.html" # TemplateView의 template_name 필드를 오버라이딩 한 것
+
+# POST 처리 함수형과 클래스형의 비교
+def myview(request):
+    if request.method == "POST":
+        form = MyForm(request.POST)
+        if form.is_valid():
+            # cleand_date로 관련 로직 처리
+            return HttpResponseRedirect('/success/')
+    else:
+        form = MyForm(initial={'key': 'value'})
+
+    return render(request 'form_template.html', {'form': form})
+
+class MyFormView(View):
+    form_class = MyForm # 사용자에게 보여줄 폼을 정의한 forms.py 파일 내의 클래스명
+    initial = {'key': 'value'}
+    template_name = 'form_template.html' # 템플릿 이름
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid(): #유효한 폼 데이터로 처리할 로직코딩, super()함수를 사용하면 success_url로 지정된 URL로 리다이렉션 처리됨
+            # cleand_data로 관련 로직 처리
+            return HttpResponseRedirect('/success/')
+            
+        return render(request, self.template_name, {'form': form})
